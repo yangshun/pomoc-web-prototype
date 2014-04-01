877a98924aac10377a7e0292130602e106514861
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-04-02 03:36:25 +0800

    Added sound effect

--- public/js/utils.js
+++ public/js/utils.js
@@ -3,3 +3,11 @@ var utils = {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
+
+var ding = new Audio("/audio/ding.mp3");
+
+function playDing() {
+  ding.pause();
+  ding = new Audio("/audio/ding.mp3");
+  ding.play();
+}
